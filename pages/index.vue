6d585e58b831fc66d3e1ef1cf20b0b9564b1ce29
Hiroshi Nagayama <e13513@konicaminolta.com>
2021-07-14 15:41:10 +0900

    second commit.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,67 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    const data = await $axios.$get(
+      process.client ?
+        `${location.origin}/api/nuxtpost`
+        :
+        `/api/nuxtpost`
+    )
+    return data
+  }
+}
 </script>
+
+// <style lang="scss" scoped>
+// .main {
+//   width: 960px;
+//   margin: 0 auto;
+// }
+
+// .title {
+//   margin-bottom: 200px;
+//   padding-bottom: 200px;
+// }
+
+// .publishedAt {
+//   margin-bottom: 40px;
+// }
+
+// .post {
+//   & > h1 {
+//     font-size: 30px;
+//     font-weight: bold;
+//     margin: 40px 0 20px;
+//     background-color: #eee;
+//     padding: 10px 20px;
+//     border-radius: 5px;
+//   }
+
+//   & > h2 {
+//     font-size: 24px;
+//     font-weight: bold;
+//     margin: 40px 0 16px;
+//     border-bottom: 1px solid #ddd;
+//   }
+
+//   & > p {
+//     line-height: 1.8;
+//     letter-spacing: 0.2px;
+//   }
+
+//   & > ol {
+//     list-style-type: decimal;
+//     list-style-position: inside;
+//   }
+// }
+// </style>
